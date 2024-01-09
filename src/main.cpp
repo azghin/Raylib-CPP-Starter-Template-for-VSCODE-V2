35efd4d9836541c6cf5db23a799ce3c2f50cf501
azghin <azghinabdelkarim@gmail.com>
2024-01-09 14:43:37 +0100

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <raylib.h>
-
+#include "grid.h"
 
 int main()
 {
-    return 0;
+    Color DarkBlue={44,44,127,255};
+    InitWindow(300, 600, "raylib Tetris");
+    SetTargetFPS(60);
+
+    Grid grid= Grid();
+    grid.Print();
+    
+
+    while(WindowShouldClose()==false){
+        BeginDrawing();
+        
+        ClearBackground(DarkBlue);
+        grid.Draw();
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
